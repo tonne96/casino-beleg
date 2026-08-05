0c2f20192eecec1106fbc52efbc8f478284cc8f4
Lea380 <182086663+lea380@users.noreply.github.com>
2026-08-05 12:00:22 +0200

    Fix Rules

--- roulette-service/src/main/java/beleg/rouletteservice/view/request/PlayRequestDto.java
+++ roulette-service/src/main/java/beleg/rouletteservice/view/request/PlayRequestDto.java
@@ -25,16 +25,16 @@ public record PlayRequestDto(
 
         @Schema(description = """
                 Zahl(en), deren Bedeutung vom gewaehlten bet_type abhaengt:
-                - SINGLE: [zahl] - genau eine Zahl, 0 bis 36
+                - SINGLE: [a] - genau eine Zahl, a aus 0 bis 36
                 - RED_OR_BLACK: [0|1] - 0 = Schwarz, 1 = Rot
                 - ODD_OR_EVEN: [0|1] - 0 = Gerade, 1 = Ungerade
                 - LOW_OR_HIGH: [0|1] - 0 = Niedrig (1-18), 1 = Hoch (19-36)
                 - DOZEN: [1|2|3] - 1., 2. oder 3. Dutzend
                 - COLUMN: [1|2|3] - 1., 2. oder 3. Spalte
-                - STREET: [a] - Start einer 3er-Reihe, a aus {1,4,7,...,34}
-                - SIX_LINE: [a] - Start einer 6er-Reihe, a aus {1,4,7,...,31}
-                - CORNER: [a] - linke obere Ecke eines Vierecks, a 1-32, a modulo 3 ungleich 0
-                - SPLIT: [a, b] - zwei auf dem Tableau echt benachbarte Zahlen, je 1-36
+                - STREET: [a] - Start einer 3er-Reihe, a aus {1,4,7,...,34} 1. Spalte
+                - SIX_LINE: [a] - Start einer 6er-Reihe, a aus {1,4,7,...,31} 1. Spalte
+                - CORNER: [a] - linke obere Ecke eines Vierecks, a modulo 3 ungleich 0, a aus 1. oder 2. Spalte
+                - SPLIT: [a, b] - zwei auf dem Tableau echt benachbarte Zahlen, je 0-36
                 Ausfuehrliche Erklaerung: GET /casino/roulette/api/info/rules
                 """,
                 example = "[17]")
